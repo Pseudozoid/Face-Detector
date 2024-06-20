a7719f41898cdea87697f8e61efae7b4eea07f44
Pseudozoid <sarathmadhavr@gmail.com>
2024-06-20 23:12:21 +0530

    added led

--- face_detector.ino
+++ face_detector.ino
@@ -2,11 +2,13 @@
 #include <Adafruit_LiquidCrystal.h>
 
 Adafruit_LiquidCrystal lcd_1(0);
+const int ledPin = 13;
 
 void setup() {
   lcd_1.begin(16, 2);
-  lcd_1.setBacklight(1);
-  Serial.begin(9600);
+  lcd_1.setBacklight(1); 
+  Serial.begin(9600); 
+  pinMode(ledPin, OUTPUT);
   lcd_1.print("Waiting...");
 }
 
@@ -16,5 +18,11 @@ void loop() {
     lcd_1.clear();
     lcd_1.setCursor(0, 0);
     lcd_1.print(message);
+
+    if (message == "Face Detected!") {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
   }
 }
